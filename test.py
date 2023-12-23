e852cad6a18eadf0e05bd0a61f3f0e494a0699df
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-12-23 10:08:22 +0530

    Data fetching implemented

---
+++ test.py
@@ -0,0 +1,35 @@
+
+from trade import DataFetcher
+
+
+
+# CONFIGURABLE PARAMETERS
+SYMBOL = 'TATAMOTORS.NS'
+PERIOD_FOR_DATA_COLLECTION = '2y'
+INTERVAL = '1d'
+INDICATOR = 'RSI'
+PERIOD_FOR_CALCULATION = 14
+FAST_PERIOD = 12 
+SLOW_PERIOD = 26
+SIGNAL_PERIOD = 9
+K_PERIOD = 14
+D_PERIOD = 4
+LOOKBACK = 5
+TIMEPERIOD = 50
+
+
+# HISTORICAL DATA COLLECTION
+historical_data, closure_values = DataFetcher(symbol=SYMBOL, period=PERIOD_FOR_DATA_COLLECTION, interval=INTERVAL).fetchClosureHistoryData()
+
+
+
+    
+
+
+
+
+
+
+
+
+

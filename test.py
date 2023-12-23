f464dd7eeeffd7bc1ea0b882027df6b8636f007a
AbhijeetSrivastav <abhijeetsrivastav292@gmail.com>
2023-12-23 21:58:58 +0530

    test module updated with driver code

--- test.py
+++ test.py
@@ -8,8 +8,9 @@ from trade import IndicatorEMA
 from trade import GenerateAlert
 from trade import PushAlert
 
+
 # CONFIGURABLE PARAMETERS
-SYMBOL = 'TATAMOTORS.NS'
+SYMBOL = 'Reliance.NS'
 PERIOD_FOR_DATA_COLLECTION = '2y'
 INTERVAL = '1d'
 INDICATOR = 'RSI'
@@ -21,30 +22,52 @@ K_PERIOD = 14
 D_PERIOD = 4
 LOOKBACK = 5
 TIMEPERIOD = 50
+LONG_TIMEPERIOD = 200
+SHORT_TIMEPERIOD = 50
 
 
 # HISTORICAL DATA COLLECTION
 historical_data, closure_values, low_values, high_values = DataFetcher(symbol=SYMBOL, period=PERIOD_FOR_DATA_COLLECTION, interval=INTERVAL).fetchClosureHistoryData()
 
 
-# INDICATOR CALCULATION
-# >> RSI
-rsi_values = IndicatorRSI(closure_value=closure_values, period=PERIOD_FOR_CALCULATION).rsi
 
-#>> MACD
-macd_values = IndicatorMACD(closure_value=closure_values, fastperiod=FAST_PERIOD, slowperiod=SLOW_PERIOD, signalperiod=SIGNAL_PERIOD).macd
+# -------------TEST CASES----------------
 
-#>> STOCH
-stoch_values = IndicatorSTOCH(closure_value=closure_values, low_values=low_values, high_values=high_values, kperiod=K_PERIOD, dperiod=D_PERIOD, lookback=LOOKBACK).stoch
+# # >> CASE: RSI
+# rsi_suggestions, rsi_values = IndicatorRSI(closure_value=closure_values, period=PERIOD_FOR_CALCULATION).cal_suggestions()
 
-#>> SMA
-sma_values = IndicatorSMA(closure_value=closure_values, timeperiod=TIMEPERIOD).sma
+# alert_messages = GenerateAlert(indicator='RSI', indicator_suggestions=rsi_suggestions, indicator_values=rsi_values, historical_data=historical_data).alerts
 
-#>> EMA
-ema_values = IndicatorEMA(closure_value=closure_values, timeperiod=TIMEPERIOD).ema
+# PushAlert(alert_messages=alert_messages).pushConsole()
 
-# Dummy
-alert_messages = GenerateAlert(indicator='RSI', indicator_values=rsi_values, historical_data=historical_data, symbol=SYMBOL, debug=False).alert_messages
 
-PushAlert(alert_messages=alert_messages).pushConsole()
+# # >> CASE: MACD
+# macd_suggestions, macd_hist_values = IndicatorMACD(closure_value=closure_values, fastperiod=FAST_PERIOD, slowperiod=SLOW_PERIOD, signalperiod=SIGNAL_PERIOD).cal_suggestions()
 
+# alert_messages = GenerateAlert(indicator='MACD', indicator_suggestions=macd_suggestions, indicator_values=macd_hist_values, historical_data=historical_data).alerts
+
+# PushAlert(alert_messages=alert_messages).pushConsole()
+
+
+# # >> CASE: STOCH
+# stoch_suggestions, stoch_values = IndicatorSTOCH(closure_value=closure_values, low_values=low_values, high_values=high_values, kperiod=K_PERIOD, dperiod=D_PERIOD, lookback=LOOKBACK).cal_suggestions()
+
+# alert_messages = GenerateAlert(indicator='STOCH', indicator_suggestions=stoch_suggestions, indicator_values=stoch_values, historical_data=historical_data).alerts
+
+# PushAlert(alert_messages=alert_messages).pushConsole()
+
+
+# # >> CASE: SMA
+# sma_suggestions, sma_values = IndicatorSMA(closure_value=closure_values, short_sma_timeperiod=SHORT_TIMEPERIOD, long_sma_timeperiod=LONG_TIMEPERIOD).cal_suggestions()
+
+# alert_messages = GenerateAlert(indicator='SMA', indicator_suggestions=sma_suggestions, indicator_values=sma_values, historical_data=historical_data).alerts
+
+# PushAlert(alert_messages=alert_messages).pushConsole()
+
+
+# # >> CASE: EMA
+# ema_suggestions, ema_values = IndicatorEMA(closure_value=closure_values, short_ema_timeperiod=SHORT_TIMEPERIOD, long_ema_timeperiod=LONG_TIMEPERIOD).cal_suggestions()
+
+# alert_messages = GenerateAlert(indicator='EMA', indicator_suggestions=ema_suggestions, indicator_values=ema_values, historical_data=historical_data).alerts
+
+# PushAlert(alert_messages=alert_messages).pushConsole()
